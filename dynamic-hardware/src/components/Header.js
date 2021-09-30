2752146724a21ac4ab7707f35401828832e7580f
Giovanni Romaña Cuesta <smithgiovanni53@gmail.com>
2021-09-29 20:03:51 -0500

    inicio de creacion del header

---
+++ dynamic-hardware/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'styles/header-styles';
+const Header = () => {
+  return (
+    <header className="header">
+      hola
+      <div className="nombre-logo"></div>
+    </header>
+  );
+};
+
+export default Header;
