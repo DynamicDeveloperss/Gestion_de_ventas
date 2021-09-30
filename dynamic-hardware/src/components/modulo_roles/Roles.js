d935a432fde733197da049caaaca5bc34af15e96
carolina <caritonaranjo@gmail.com>
2021-09-30 15:30:08 -0500

    correccion de estilos y los import para que funcione el componente en react

--- dynamic-hardware/src/components/modulo_roles/Roles.js
+++ dynamic-hardware/src/components/modulo_roles/Roles.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import './roles.css';
+import logo from './Icono.png';
 
 const Roles = () => {
     return (
-        <div>
-            <main className="main">
+        <body className = "grid-container">
+        <main className="main">
             <section  className="main-logo"> 
-                <img src="./src/Imagenes y diseño/Imagenes/Icono.png" alt="Logo" class="logo-Grande">
+                <img src={logo} alt="Logo" className="logo-Grande"/>
                 <h1 className="titulo-principal">NEW ANDROID</h1>
             </section>
             <section>    
@@ -13,43 +15,44 @@ const Roles = () => {
                     <h3 className="subtitulos">REGISTRO USUARIOS</h3>
                     <br/>
                         <label for="apellidos">Apellidos </label>
-                        <input type="text" name="apellidos" placeholder="Perez Perez" />
+                        <input type="text" name="apellidos" placeholder="Perez Perez"></input>
                         <label for="nombre">Nombres </label>
-                        <input  type="text" name="nombre" placeholder="Pepito " />
+                        <input  type="text" name="nombre" placeholder="Pepito "></input>
                         <label for="email">Email</label>
-                        <input type="email" placeholder="Email del empleado" required>                
+                        <input type="email" placeholder="Email del empleado" required></input>                
                         <label for="cargo"> Cargo </label>
-                        <input name="sueldo" placeholder="vendedor" type="text" />
+                        <input name="sueldo" placeholder="vendedor" type="text"></input>
                         <label for="sueldo"> Sueldo </label>
-                        <input name="sueldo" placeholder="10000"  type="number"/>
+                        <input name="sueldo" placeholder="10000"  type="number"></input>
                         <label for="turno">Turno</label>
-                        <input name="turno" placeholder="Diurno" type="text" />
+                        <input name="turno" placeholder="Diurno" type="text"></input>
                         <label for="codigo">Identificacion </label>
-                        <input name="identificacion" placeholder="1012345679" type="number"/>
+                        <input name="identificacion" placeholder="1012345679" type="number"></input>
                         <section>
-                    <h3 class="subtitulos">Roles de usuario</h3>
-                        <select name="roles" id="rol" class="roles">
+                    <h3 className="subtitulos">Roles de usuario</h3>
+                        <select name="roles" id="rol" className="roles">
                             <option value="administrador">Administrador</option>
                             <option value="vendedor">Vendedor</option>
                             <option value="usuario">Usuario</option>
                         </select>  
 
-                    <h3 class="subtitulos">Estado del Usuario</h3>
-                    <select name="estado" id="rol" class="roles">
+                    <h3 className="subtitulos">Estado del Usuario</h3>
+                    <select name="estado" id="rol" className="roles">
                         <option value="Pendiente">Pendiente</option>
                         <option value="autorizado">Autorizado</option>
                         <option value="no autorizado">No Autorizado</option>
                     </select>  
-                </section>      
+            </section>      
                     <br/>        
-                <button type="submit" class="btn btn-verde">Enviar Datos</button>
-                <button type="reset" class="btn btn-verde" >Reiniciar</button>
+                    <button type="submit" className="btn btn-verde">Enviar Datos</button>
+                    <button type="reset" className="btn btn-verde" >Reiniciar</button>
             
-                </form>
+                    </form>
             </section>
-    </main>  
-        </div>
-    )
-}
+        </main> 
+        </body> 
+    
+    );
+};
 
 export default Roles
