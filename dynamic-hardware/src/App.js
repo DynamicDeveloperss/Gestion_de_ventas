8301d341bf8ef6e8616834eb32f8ba655c195e32
Cindy Guzman <cindyortiz12323@gmail.com>
2021-10-02 14:16:59 -0500

    Se suben cambios realizados

--- dynamic-hardware/src/App.js
+++ dynamic-hardware/src/App.js
@@ -1,10 +1,9 @@
-import Vendedores from "./components/ejemplo/modulo-vendedores/Vendedores";
-import Login from "./components/ejemplo/login/Login";
+import Vendedores from "./components/modulo-vendedores/Vendedores";
+import Login from "./components/login/Login";
 import { Productos } from "./components/AdmdeProductos/Productos";
 import Roles from './components/modulo_roles/Roles';
 import Ventas from './components/ventas/Ventas';
-import './components/modulo_roles/roles.css';
-import './App.css';
+import Header from "./components/header/Header";
 
 function App() {
   return (
