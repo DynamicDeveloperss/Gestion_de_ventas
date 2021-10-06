d9a86323721e5a4d763564f3fd745c217eae272e
Giovanni Romaña Cuesta <smithgiovanni53@gmail.com>
2021-10-06 10:37:23 -0500

    agregada funcion para capturar los datos del formulario y almacenarlos temporalmente en un objeto

--- dynamic-hardware/src/App.js
+++ dynamic-hardware/src/App.js
@@ -1,24 +1,16 @@
-import Vendedores from "./components/modulo-vendedores/Vendedores";
-import Login from "./components/login/Login";
-import { Productos } from "./components/AdmdeProductos/Productos";
+import Vendedores from './components/modulo-vendedores/Vendedores';
+import Login from './components/login/Login';
+import { Productos } from './components/AdmdeProductos/Productos';
 import Roles from './components/modulo_roles/Roles';
 import Ventas from './components/ventas/Ventas';
-import Header from "./components/header/Header";
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <div> 
-      <Ventas/>
-      <Login/>
-      <Productos/>
-      <Roles/>
-      <Vendedores/>
+    <div>
+      <Ventas />
     </div>
   );
-};
-
-
-
+}
 
 export default App;
-
--- dynamic-hardware/src/components/ventas/Ventas.js
+++ dynamic-hardware/src/components/ventas/Ventas.js
@@ -1,49 +1,105 @@
-
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Header from '../header/Header';
 import './ventas.css';
 import logo from './Icono.png';
 
-
 const Ventas = () => {
+  const datosIniciales = [
+    {
+      id_vendedor: '123456',
+      cedula: '123456',
+      productos: 'Monitor 4k',
+      valor: 1000000,
+      fecha_venta: '25/03/2021',
+      id_vendedor: '12345678',
+    },
+    {
+      id_vendedor: '123456',
+      cedula: '123456',
+      productos: 'Monitor 4k',
+      valor: 1000000,
+      fecha_venta: '25/03/2021',
+      id_vendedor: '12345678',
+    },
+    {
+      id_vendedor: '123456',
+      cedula: '123456',
+      productos: 'Monitor 4k',
+      valor: 1000000,
+      fecha_venta: '25/03/2021',
+      id_vendedor: '12345678',
+    },
+    {
+      id_vendedor: '123456',
+      cedula: '123456',
+      productos: 'Monitor 4k',
+      valor: 1000000,
+      fecha_venta: '25/03/2021',
+      id_vendedor: '12345678',
+    },
+  ];
+
+  const formVenta = useRef(null);
+  const [datos, setDatos] = useState(datosIniciales);
+  const enviarDatos = (e) => {
+    e.preventDefault(); // Evita que el navegador refresque la página luego de un submit
+    const fd = new FormData(formVenta.current); // Aqui se almacena la informacion del formulario
+    const nuevaVenta = {};
+    fd.forEach((item, key) => {
+      nuevaVenta[key] = item;
+    });
+    console.log(nuevaVenta);
+  };
+
   return (
     <div>
       <Header />
       <main class="main">
-      <section  className="main-logo"> 
-                <img src={logo} alt="Logo" className="logo-Grande"/>
-                <h1 className="titulo-principal">DYNAMIC HARDWARE</h1>
-      </section>
+        <section className="main-logo">
+          <img src={logo} alt="Logo" className="logo-Grande" />
+          <h1 className="titulo-principal">DYNAMIC HARDWARE</h1>
+        </section>
         <section>
-          <form action="" class="forms">
+          <form ref={formVenta} onSubmit={enviarDatos} class="forms">
             <h3 class="subtitulos">Información del Comprador</h3>
-            <input type="text" placeholder="Nombre" name="nombre" />
-            <input type="text" placeholder="Cédula" name="cedula" />
-            <input type="email" placeholder="Correo Electrónico" name="correo" />
-            <input type="text" placeholder="Teléfono" name="telefono" />
+            <input type="text" placeholder="Nombre" name="nombre" required />
+            <input type="text" placeholder="Cédula" name="cedula" required />
+            <input type="email" placeholder="Correo Electrónico" name="correo" required />
+            <input type="text" placeholder="Teléfono" name="telefono" required />
             <h3 class="subtitulos">Información de la Venta</h3>
             <label for="id-vendedor">ID Vendedor</label>
-            <input type="text" id="id-vendedor" placeholder="ID Vendedor" name="id-vendedor" />
+            <input
+              type="text"
+              id="id-vendedor"
+              placeholder="ID Vendedor"
+              name="id-vendedor"
+              required
+            />
             <label for="">ID Producto</label>
-            <input type="text" placeholder="ID Producto" name="id-producto" />
+            <input type="text" placeholder="ID Producto" name="id-producto" required />
             <label for="">Cantidad</label>
-            <input type="number" placeholder="Cantidad" name="cantidad" />
+            <input type="number" placeholder="Cantidad" name="cantidad" required />
             <label for="">Valor de la Venta</label>
-            <input type="text" placeholder="Valor" name="valor" />
+            <input type="text" placeholder="Valor" name="valor" required />
             <label for="fecha-venta">Fecha de Venta</label>
-            <input type="date" id="fecha-venta" placeholder="Fecha de Venta" name="fecha-venta" />
+            <input
+              type="date"
+              id="fecha-venta"
+              placeholder="Fecha de Venta"
+              name="fecha-venta"
+              required
+            />
             <button class="btn btn-verde">Agregar Venta</button>
           </form>
         </section>
         <section class="section-tabla">
-          <h3 class="subtitulos">Registro de Ventas</h3>
+          <h3 class="subtitulos">Historico de Ventas</h3>
           <div class="input-buscar">
             <input type="text" placeholder="Buscar" />
             <i class="fas fa-search"></i>
           </div>
           <table class="tabla-ventas">
             <thead>
-              <th></th>
               <th>ID Venta</th>
               <th>Cédula Comprador</th>
               <th>Productos</th>
@@ -52,27 +108,27 @@ const Ventas = () => {
               <th>ID Vendedor</th>
               <th>Estado</th>
             </thead>
-            <tr>
-              <td>
-                <input type="checkbox" name="" id="" />
-              </td>
-              <td>Lorem</td>
-              <td>Lorem</td>
-              <td>Lorem</td>
-              <td>Lorem</td>
-              <td>Lorem</td>
-              <td>Lorem</td>
-              <td>
-                <select name="estado" class="combo-estado">
-                  <option value="0">Estado...</option>
-                  <option value="entregado">Entregado</option>
-                  <option value="en proceso">En proceso</option>
-                  <option value="cancelada">Cancelada</option>
-                </select>
-              </td>
-            </tr>
+            {datosIniciales.map((item, key) => {
+              return (
+                <tr>
+                  <td>{item.id_vendedor}</td>
+                  <td>{item.cedula}</td>
+                  <td>{item.productos}</td>
+                  <td>{item.valor}</td>
+                  <td>{item.fecha_venta}</td>
+                  <td>{item.id_vendedor}</td>
+                  <td>
+                    <select name="estado" class="combo-estado">
+                      <option value="0">Estado...</option>
+                      <option value="entregado">Entregado</option>
+                      <option value="en proceso">En proceso</option>
+                      <option value="cancelada">Cancelada</option>
+                    </select>
+                  </td>
+                </tr>
+              );
+            })}
           </table>
-          <button class="btn btn-rojo">Eliminar Venta</button>
         </section>
       </main>
     </div>
