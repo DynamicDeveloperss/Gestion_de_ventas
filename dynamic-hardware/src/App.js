af1043b0363af737c4367aaf3690a636e25c8634
nibague <nikolasibagues@gmail.com>
2021-10-11 15:58:54 -0500

    agregando componentes en modulo

--- dynamic-hardware/src/App.js
+++ dynamic-hardware/src/App.js
@@ -1,12 +1,23 @@
-import Vendedores from "./components/ejemplo/modulo-vendedores/Vendedores";
+import Vendedores from "./components/modulo-vendedores/Vendedores";
+import Login from "./components/login/Login";
+import { Productos } from "./components/AdmdeProductos/Productos";
+import Roles from './components/modulo_roles/Roles';
+import Ventas from './components/ventas/Ventas';
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div> 
+      <Ventas/>
+      <Login/>
+      <Productos/>
+      <Roles/>
       <Vendedores/>
-      
     </div>
   );
-}
+};
+
+
+
 
 export default App;
