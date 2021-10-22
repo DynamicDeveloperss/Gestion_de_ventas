7cb8e8e0ecc974b9b130942451c35f2d27991580
Giovanni Romaña Cuesta <smithgiovanni53@gmail.com>
2021-10-22 17:29:24 -0500

    rutas borradas de app.js

--- dynamic-hardware/src/App.js
+++ dynamic-hardware/src/App.js
@@ -1,16 +1,3 @@
-import Vendedores from './components/modulo-vendedores/Vendedores';
-import Login from './components/login/Login';
-import { Productos } from './components/AdmdeProductos/Productos';
-import Roles from './components/modulo_roles/Roles';
-import Header from './components/header/Header';
-import Ventas from './components/ventas/Ventas';
-
-function App() {
-  return (
-    <div>
-      <Ventas />
-    </div>
-  );
-}
+function App() {}
 
 export default App;
