76c2cf4de2a3a2c7352782b19dba0d1eb05670cf
Cindy Guzman <cindyortiz12323@gmail.com>
2021-10-22 19:30:51 -0500

    Se suben cambiios faltantes

--- dynamic-hardware/src/App.js
+++ dynamic-hardware/src/App.js
@@ -1,24 +1,24 @@
-import Vendedores from "./components/modulo-vendedores/Vendedores";
 import Login from "./components/login/Login";
+import { Component } from "react";
+import Vendedores from "./components/modulo-vendedores/Vendedores";
 import { Productos } from "./components/AdmdeProductos/Productos";
 import Roles from './components/modulo_roles/Roles';
 import Ventas from './components/ventas/Ventas';
-import Header from "./components/header/Header";
 
-function App() {
-  return (
-    <div> 
-      <Ventas/>
-      <Login/>
-      <Productos/>
-      <Roles/>
-      <Vendedores/>
-    </div>
-  );
-};
+class App extends Component {
 
-
-
+  render() {
+    return (
+      <div className="App container">
+        <Login/>
+        <Ventas/>
+        <Productos/>
+        <Roles/>
+        <Vendedores/>
+      </div>
+    );
+  }
+}
 
 export default App;
 
--- dynamic-hardware/src/components/login/Login.js
+++ dynamic-hardware/src/components/login/Login.js
@@ -1,18 +1,58 @@
-import React from 'react'
+import React, { Component } from "react";
 import './Login.css'
 import logo from './Icono.png'
 import computador from './Foto Login.jpg'
+import { GoogleLogin, GoogleLogout } from "react-google-login";
 
-const Login = () => {
-    return (
-        <div>
+const CLIENT_ID = "788811814863-doldm5d1gdgpgp1qj9vkeq7m8qnbrs9d.apps.googleusercontent.com"
+
+export default class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            isLoggedIn: false,
+            userInfo: {
+              name: "",
+              emailId: "",
+            },
+          };
+        }
+      
+    // Success Handler
+    responseGoogleSuccess = (response) => {
+        console.log();
+        let userInfo = {
+            name: response.profileObj.name,
+            emailId: response.profileObj.email,
+        };
+        this.setState({ userInfo, isLoggedIn: true });
+    };
+      
+    // Error Handler
+    responseGoogleError = (response) => {
+        console.log(response);
+    };
+      
+    // Logout Session and Update State
+    logout = (response) => {
+        console.log(response);
+        let userInfo = {
+            name: "",
+            emailId: "",
+        };
+        this.setState({ userInfo, isLoggedIn: false });
+    };
+    
+    render() {
+        return (
+            <div>
                 <div className="contenedor">
                     <div className="login-img">
-                        <img src={computador} />
+                        <img src={computador} alt="Imagen lateral" />
                     </div>
                     <div className="section_right">
                         <div className="section_right_container">
-                            <img src={logo} width="100" height="100" />
+                            <img src={logo} width="100" height="100" alt="Logo" />
                         </div>
                         <div classNameName="section_right_container_form">
                             <form>
@@ -25,14 +65,35 @@ const Login = () => {
                                     </div>
                                     <div className="content-button" id="lower">
                                         <button type="submit" className="btn">Login</button>
+                                        <div className="row mt-5">
+                                            <div className="col-md-12">
+                                                {this.state.isLoggedIn ? (
+                                                    <div>
+                                                     <h1>Welcome, {this.state.userInfo.name}</h1>
+                                                        <GoogleLogout
+                                                        clientId={CLIENT_ID}
+                                                        onLogoutSuccess={this.logout}
+                                                        ></GoogleLogout>
+                                                    </div>
+                                                    ) : (
+                                                    <GoogleLogin
+                                                    clientId={CLIENT_ID}
+                                                    buttonText="Sign In with Google"
+                                                    onSuccess={this.responseGoogleSuccess}
+                                                    onFailure={this.responseGoogleError}
+                                                    isSignedIn={true}
+                                                    cookiePolicy={"single_host_origin"}
+                                                    />
+                                                )}
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </form>
                         </div>
                     </div>
                 </div>
-        </div>
-    )
+            </div>
+        )
+    }
 }
-
-export default Login
